578775c3ec81d4382c69322dce1e697d07c0789f
gktrk07 <43420902+gktrk07@users.noreply.github.com>
2023-10-07 02:32:27 +0300

    Delete projj directory

--- projj/lib/Configuration.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "SerialComm.h"
-
-#define SC_PORT_NAME "/dev/ttyS1"
-#define SC_BAUDRATE BAUDRATE_115200
-#define SC_DATABIT DATABIT_8
-#define SC_STOPBIT STOPBIT_1
-#define SC_PARITYBIT PARITYBIT_NONE
